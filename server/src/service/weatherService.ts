cabe0c0d93217a33f19a19ae072fe849bc8e622e
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-08-24 22:04:49 -0400

    added weatherService.ts: coordinates interface, weather class. still working on weatherService methods. added htmlRoutes.ts

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -2,30 +2,78 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // TODO: Define an interface for the Coordinates object
-
+interface Coordinates {
+  lat: number;
+  lon: number;
+}
 // TODO: Define a class for the Weather object
+class Weather {
+  city: string;
+  date: string;
+  icon: string;
+  description: string;
+  temp: number;
+  wind: number;
+  humidity: number;
 
+  constructor(
+    city: string,
+    date: string,
+    icon: string,
+    description: string,
+    temp: number,
+    wind: number,
+    humidity: number
+  ) {
+    this.city = city;
+    this.date = date;
+    this.icon = icon;
+    this.description = description;
+    this.temp = temp;
+    this.wind = wind;
+    this.humidity = humidity;
+  }
+}
 // TODO: Complete the WeatherService class
 class WeatherService {
-  // TODO: Define the baseURL, API key, and city name properties
+  // TODO: Define the baseURL, API key, and geoURL properties
+  baseURL: string;
+  apiKey: string;
+  geoURL: string; //what to do with this?
+
+  constructor() {
+    this.baseURL = process.env.API_BASE_URL || '';
+    this.apiKey = process.env.API_KEY || '';
+    this.geoURL = process.env.GEO_URL || '';
+  }
   // TODO: Create fetchLocationData method
-  // private async fetchLocationData(query: string) {}
+  private async fetchLocationData(query: string) {
+    let response = await fetch(`${this.geoURL}${query}&limit=5&appid=${this.apiKey}`);
+    let geoLocationData = await response.json();  
+    this.destructureLocationData(geoLocationData[0]);
+  }
   // TODO: Create destructureLocationData method
-  // private destructureLocationData(locationData: Coordinates): Coordinates {}
+  private destructureLocationData(locationData: Coordinates): Coordinates {
+    let coordinates: Coordinates = {
+      lat: locationData.lat,
+      lon: locationData.lon,
+    };
+    return coordinates;
+  }
   // TODO: Create buildGeocodeQuery method
-  // private buildGeocodeQuery(): string {}
+  private buildGeocodeQuery(): string {}
   // TODO: Create buildWeatherQuery method
-  // private buildWeatherQuery(coordinates: Coordinates): string {}
+  private buildWeatherQuery(coordinates: Coordinates): string {}
   // TODO: Create fetchAndDestructureLocationData method
-  // private async fetchAndDestructureLocationData() {}
+  private async fetchAndDestructureLocationData() {}
   // TODO: Create fetchWeatherData method
-  // private async fetchWeatherData(coordinates: Coordinates) {}
+  private async fetchWeatherData(coordinates: Coordinates) {}
   // TODO: Build parseCurrentWeather method
-  // private parseCurrentWeather(response: any) {}
+  private parseCurrentWeather(response: any) {}
   // TODO: Complete buildForecastArray method
-  // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
+  private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
   // TODO: Complete getWeatherForCity method
-  // async getWeatherForCity(city: string) {}
+  async getWeatherForCity(city: string) {}
 }
 
 export default new WeatherService();
